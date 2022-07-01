0ca7c604bd8bd741fd933447d0e8788228b795ce
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-07-01 17:27:41 +0100

    isBalanced added

--- cls_stack/cls_stack.py
+++ cls_stack/cls_stack.py
@@ -1,19 +1,41 @@
 class Stack:
 
-    def __init__(self):
-        self.items = []
+    def __init__(self, sample: str):
+        self.stack_list = list(sample)
 
     def isEmpty(self):
-        return self.items == []
+        return self.stack_list == []
 
     def push(self, item):
-        self.items.append(item)
+        self.stack_list.append(item)
 
     def pop(self):
-        return self.items.pop()
+        return self.stack_list.pop()
 
     def peek(self):
-        return self.items[len(self.items) - 1]
+        return self.stack_list[len(self.stack_list) - 1]
 
     def size(self):
-        return len(self.items)
+        return len(self.stack_list)
+
+    def isBalanced(self, sample_stack: list):
+        half_stack = len(sample_stack) / 2
+
+        for idx in range(0, half_stack):
+            if sample_stack[idx] != sample_stack[len(sample_stack) - idx]:
+                return 'Unbalanced'
+        return 'Balanced'
+
+    def isBalanced(self):
+
+        if len(self.stack_list) % 2 != 0:
+            return 'Unbalanced'
+
+        half_stack = int(len(self.stack_list)) / 2
+        print(f'half_stack = {half_stack}')
+
+        for idx in range(0, int(half_stack)):
+            if self.stack_list[idx] != self.stack_list[len(self.stack_list)-1 - idx]:
+                print(f'{self.stack_list[idx]} != {self.stack_list[len(self.stack_list)-1 - idx]}')
+                return 'Unbalanced'
+        return 'Balanced'
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from cls_stack.cls_stack import Stack
 
 
 if __name__ == '__main__':
-    ...
+    first_str =  Stack('(((([{}]))))')
+    print(first_str.isBalanced())
 
